f33d7f68b6ee45555a6fe1dad2565b581616413b
Matthew Dunbar <matthewdunbar11@gmail.com>
2017-06-19 12:55:47 -0400

    RPN calculator addition, basic input count error handling, and tests

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -1,4 +1,28 @@
+require_relative './incorrect_inputs_error'
+
 class RPNCalculator
 
+	def initialize
+		@input_items = []
+	end
 
+	def Input(text)
+		if text =~ /\A-?[0-9]*(\.[0-9]*)?\Z/ #regex to determine if numeric with optional negative and decimal
+			if @input_items.count > 1
+				raise IncorrectInputsError, "More than two numbers were provided"
+			else
+				@input_items.push(text.to_f)
+			end
+		elsif text == "+"
+			if @input_items.count != 2
+				raise IncorrectInputsError, "Only one number was provided"
+			else
+				@result = @input_items.pop + @input_items.pop				
+			end
+		end
+	end
+
+	def Result
+		@result
+	end
 end
--- spec/rpn_calculator_spec.rb
+++ spec/rpn_calculator_spec.rb
@@ -1,7 +1,32 @@
 require_relative '../rpn_calculator'
+require_relative '../incorrect_inputs_error'
+
 
 describe RPNCalculator do
-	it 'should load the class' do
-		expect(1).to eq(1)
+	before :each do
+		@rpn_calculator = RPNCalculator.new
+	end
+
+	it 'should correctly add two numbers' do
+
+		@rpn_calculator.Input('4')
+		@rpn_calculator.Input('5')
+		@rpn_calculator.Input('+')
+		expect(@rpn_calculator.Result).to eq(9)
+	end
+
+	it 'should throw error if only one number added' do
+		@rpn_calculator.Input('4')
+		expect {
+			@rpn_calculator.Input('+')
+		}.to raise_exception IncorrectInputsError
+	end
+
+	it 'shoudl throw error if more than two numbers are input' do
+		@rpn_calculator.Input('4')
+		@rpn_calculator.Input('6')
+		expect {
+			@rpn_calculator.Input('7')
+		}.to raise_exception IncorrectInputsError
 	end
 end
