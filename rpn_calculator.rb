52cb5331200bbbf4b4bb16da61b3d86673d2e5cb
Matthew Dunbar <matthewdunbar11@gmail.com>
2017-06-19 18:55:32 -0400

    Adds multiplying, subtraction, better erro handling, and improves number detection

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -4,21 +4,35 @@ class RPNCalculator
 
 	def initialize
 		@input_items = []
+		@result = 0
 	end
 
 	def Input(text)
-		if text =~ /\A-?[0-9]*(\.[0-9]*)?\Z/ #regex to determine if numeric with optional negative and decimal
-			if @input_items.count > 1
-				raise IncorrectInputsError, "More than two numbers were provided"
-			else
-				@input_items.push(text.to_f)
+		begin
+			value = Float(text)
+			@input_items.push(value)
+
+			if(@input_items.count > 2)
+				@input_items.drop(1)
 			end
-		elsif text == "+"
-			if @input_items.count != 2
-				raise IncorrectInputsError, "Only one number was provided"
+		rescue ArgumentError #This means we didnt' receive a float, we probably received an arithmetic operator
+			if @input_items.count == 1
+				@val1 = @result
+				@val2 = @input_items.pop
 			else
-				@result = @input_items.pop + @input_items.pop				
+				@val2 = @input_items.pop
+				@val1 = @input_items.pop
 			end
+
+			case text
+			when "+"
+				@result = @val1 + @val2
+			when "*"
+				@result = @val1 * @val2
+			when "-"
+				@result = @val1 - @val2
+			end
+
 		end
 	end
 
--- spec/rpn_calculator_spec.rb
+++ spec/rpn_calculator_spec.rb
@@ -15,18 +15,90 @@ describe RPNCalculator do
 		expect(@rpn_calculator.Result).to eq(9)
 	end
 
-	it 'should throw error if only one number added' do
+	it 'should assume starting result of 0 if only one number added' do
 		@rpn_calculator.Input('4')
-		expect {
-			@rpn_calculator.Input('+')
-		}.to raise_exception IncorrectInputsError
+		@rpn_calculator.Input('+')
+		
+		expect(@rpn_calculator.Result).to eq(4)
+	end
+	
+
+	it 'should add two most recent numbers if more than two numbers are entered' do
+		@rpn_calculator.Input('4')
+		@rpn_calculator.Input('3')
+		@rpn_calculator.Input('10')
+		@rpn_calculator.Input('+')
+
+		expect(@rpn_calculator.Result).to eq(13)
 	end
 
-	it 'shoudl throw error if more than two numbers are input' do
-		@rpn_calculator.Input('4')
+	it 'should add a third number sent after addition' do
+		@rpn_calculator.Input('5')
 		@rpn_calculator.Input('6')
-		expect {
-			@rpn_calculator.Input('7')
-		}.to raise_exception IncorrectInputsError
+		@rpn_calculator.Input('+')
+		@rpn_calculator.Input('7')
+		@rpn_calculator.Input('+')
+
+		expect(@rpn_calculator.Result).to eq(18)
+	end
+
+	it 'should multiply integers' do
+		@rpn_calculator.Input('3')
+		@rpn_calculator.Input('6')
+		@rpn_calculator.Input('*')
+
+		expect(@rpn_calculator.Result).to eq(18)
+	end
+
+	it 'should multiply decimals' do
+		@rpn_calculator.Input('2.5')
+		@rpn_calculator.Input('5.4')
+		@rpn_calculator.Input('*')
+
+		expect(@rpn_calculator.Result).to eq(13.5)
+	end
+
+	it 'should multiple one negative number' do
+		@rpn_calculator.Input('-6.1')
+		@rpn_calculator.Input('4.23')
+		@rpn_calculator.Input('*')
+
+		expect(@rpn_calculator.Result).to eq(-25.803)
+	end
+
+	it 'should multiply two negative numbers' do
+		@rpn_calculator.Input('-2.1')
+		@rpn_calculator.Input('-7.1')
+		@rpn_calculator.Input('*')
+
+		expect(@rpn_calculator.Result).to eq(14.91)		
+	end
+
+	it 'should multiply ater adding' do
+		@rpn_calculator.Input('4')
+		@rpn_calculator.Input('5')
+		@rpn_calculator.Input('+')
+		@rpn_calculator.Input('4')
+		@rpn_calculator.Input('*')
+
+		expect(@rpn_calculator.Result).to eq(36)
+	end
+
+	it 'should add after multiplying' do
+		@rpn_calculator.Input('4')
+		@rpn_calculator.Input('5')
+		@rpn_calculator.Input('*')
+		@rpn_calculator.Input('4')
+		@rpn_calculator.Input('+')
+
+		expect(@rpn_calculator.Result).to eq(24)
+	end
+
+	it 'should subtract two numbers' do
+		@rpn_calculator.Input('4')
+		@rpn_calculator.Input('5')
+		@rpn_calculator.Input('-')
+
+		expect(@rpn_calculator.Result).to eq(-1)
 	end
 end
